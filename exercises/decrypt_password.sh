86f769d1c386c2508058c52c86b2c270faaae9f0
Emil Öhgren <boolman@gmail.com>
2018-06-01 14:44:54 +0200

    use openssl enc instead of base64 command

--- exercises/decrypt_password.sh
+++ exercises/decrypt_password.sh
@@ -4,5 +4,4 @@ set -e
 set -u
 
 server=$1
-
-nova get-password ${server} | base64 -d | openssl rsautl -decrypt -inkey ~/.ssh/id_rsa -keyform PEM
+nova get-password ${server} | openssl enc -base64 -d | openssl rsautl -decrypt -inkey ~/.ssh/id_rsa -keyform PEM
